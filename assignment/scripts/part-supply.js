e0b62b65fcd21472f880db51b3f182f49f982c50
Stephen Piper <stephen.r.piper@gmail.com>
2021-04-18 00:21:52 -0500

    finished 6

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -22,9 +22,12 @@ console.log(supplyChanges[2]);
 // 4. The last item was added by mistake. Remove it from the 'supplyChanges'
 //    array & console.log the value removed.
 console.log('4. Removed item:');
+let removeLast = supplyChanges.pop();
+console.log(removeLast);
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Adding 25 to supplyChanges.');
+supplyChanges.push(25)
 
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
@@ -32,6 +35,21 @@ console.log('5. Adding 25 to supplyChanges.');
 //    - if the value is 0, skip it. Do not log it to the console.
 //    - if the value is negative, format the log as 'Part count -x.'
 console.log('6. Showing supplyChanges...');
+
+let addCount
+let neutCount
+let subCount
+
+for (let i = 0; i < supplyChanges.length; i++) {
+  if (supplyChanges[i] > 0) { addCount = supplyChanges[i];
+    console.log(`Added ${addCount} parts.`);
+  } else if (supplyChanges[i] === 0) { neutCount = supplyChanges[i];
+    console.log();
+  } else if (supplyChanges[i] < 0) { subCount = supplyChanges[i];
+    console.log(`Part count ${subCount}.`);
+
+  }
+}
 
 
 // STRETCH GOALS
