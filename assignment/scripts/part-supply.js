0cffff2a36cb4f668f3552cf9c5671099085ff3a
Stephen Piper <stephen.r.piper@gmail.com>
2021-04-18 11:32:49 -0500

    while stretch goal done

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -56,10 +56,46 @@ for (let i = 0; i < supplyChanges.length; i++) {
 console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop.
 console.log('7. Showing supplyChanges with "for of" loop');
-
+for( change of supplyChanges){
+  if (change > 0) {
+    console.log(`Added ${change} parts.`);
+  } else if (change === 0) {
+    console.log();
+  }else if (change < 0) {
+    console.log(`Part count ${change}.`);
+  }
+}
 // 8. Rewrite the `for` loop from #6 as a `while` loop.
 console.log('8. Showing supplyChanges with "while" loop');
 
+let i = 0
+
+// while (i < supplyChanges.length) {
+//   if (supplyChanges[i] > 0); {
+//     console.log(`Added ${supplyChanges[i]} parts.`);
+//     i++;
+//   } if (supplyChanges[i] === 0) {
+//     console.log('');
+//     i++;
+//
+//   } if (supplyChanges[i] < 0) {
+//     console.log(`Part count ${supplyChanges[i]}.`);
+//     i++;
+//   }
+//
+// }
+while (i < supplyChanges.length) {
+  if (supplyChanges[i] > 0) {
+  console.log(`Added ${supplyChanges[i]} parts.`);
+  i++;
+}else if (supplyChanges[i] === 0) {
+  console.log();
+  i++;
+}else if (supplyChanges[i] < 0) {
+  console.log(`Part count ${supplyChanges[i]}`);
+  i++;
+}
+}
 
 // 9. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
